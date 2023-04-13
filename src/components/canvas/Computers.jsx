7cf0685ff817fbe4fcaf72f03b3a553bd15f2436
Toheed <toheedjamaal9@gmail.com>
2023-04-13 10:51:29 +0530

    changed gltf object path

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("../../../public/retro_pc/scene.gltf");
+  const computer = useGLTF("./retro_pc/scene.gltf");
 
   return (
     <mesh>
