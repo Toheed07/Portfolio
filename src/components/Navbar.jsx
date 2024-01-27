f61c96c5e3fce16ac7d23d7be19f1bf3ab0ae646
Toheed <toheedjamaal9@gmail.com>
2024-01-27 22:40:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
           ))}
           <li className="text-white hover:text-secondary text-[18px] font-medium cursor-pointer">
             <a
-              href="https://drive.google.com/file/d/1khvUS1Ye4-fFCbH4yxQb-MvMew_N_Veh/view?usp=sharing"
+              href="https://drive.google.com/file/d/1WJHK3gWVpxgSqH15qVmAQNKnHb2cl5JG/view?usp=sharing"
               target="_blank"
             >
               Resume
@@ -97,7 +97,7 @@ const Navbar = () => {
               ))}
               <li className="text-white hover:text-secondary text-[18px] font-medium cursor-pointer">
                 <a
-                  href="https://drive.google.com/file/d/1khvUS1Ye4-fFCbH4yxQb-MvMew_N_Veh/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1WJHK3gWVpxgSqH15qVmAQNKnHb2cl5JG/view?usp=sharing"
                   target="_blank"
                 >
                   Resume
